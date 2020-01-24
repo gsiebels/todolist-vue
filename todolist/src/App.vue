1a7a27026812be70322403745bd39ade4f024c95
Gerardo Siebels <gsiebels@gmail.com>
2020-01-24 10:29:01 +0000

    the todo app  is done

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -1,59 +1,40 @@
 <template>
   <div id="app">
-    <Header />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
-
+   <Header />
+    <router-view/>
   </div>
 </template>
 
 <script>
 import Header from './components/layout/Header'
-import Todos from './components/Todos'
-
 export default {
-  name: 'app',
+  name:'app',
   components: {
-    Header,
-    Todos
-  },
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: 'Todo One',
-          completed: false
-        },
-         {
-          id: 2,
-          title: 'Todo Two',
-          completed: false
-        },
-         {
-          id: 3,
-          title: 'Todo Three',
-          completed: false
-        }
-      ]
-    }
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id)
-    }
+    Header
   }
 }
 </script>
 
 <style>
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+}
 
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1,4;  
-  }
+#nav {
+  padding: 5px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #42b983;
+  padding-left: 5px;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
---
+++ todolist/src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h2>About</h2>
+    <p>This is the TodoList app v1.0.0. It is my first Vue app and I ❤️ it!.</p>
+  </div>
+</template>
+
+<style scoped>
+  .about {
+    text-align: center;
+    padding-top: 50px;
+  }
+  h2 {
+  padding-bottom: 30px
+  }
+</style>
